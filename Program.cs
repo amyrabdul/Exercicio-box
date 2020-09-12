37c54e23a8d5714171a16dcd73cc2b94206b5838
amyrabdul <amyrabdul@hotmail.com>
2020-09-11 23:19:06 -0300

    exercicio box

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace exercicio_box
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("+----+");
+            Console.WriteLine("|Amyr|");
+            Console.WriteLine("+----+");
+            Console.ReadKey ();
+        }
+    }
+}
